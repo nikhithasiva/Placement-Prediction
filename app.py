89f6bc10a1abbc16e511f8619ea79217155108da
nikhithasiva <61174359+nikhithasiva@users.noreply.github.com>
2021-12-09 22:36:45 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,64 @@
+# -*- coding: utf-8 -*-
+
+from flask import Flask, request, render_template
+import pickle
+
+
+# unplickling the model
+
+file = open('campusplacementpredictor.pkl', 'rb')
+rf = pickle.load(file)
+file.close()
+
+
+app = Flask(__name__)
+
+
+@app.route('/', methods=['GET', 'POST'])
+def hello_world():
+
+    if request.method == 'POST':
+
+        mydict = request.form
+        gender = int(mydict['gender'])
+        spec = int(mydict['spec'])
+        tech = int(mydict['tech'])
+        work = int(mydict['work'])
+        ssc = float(mydict['ssc'])
+        hsc = float(mydict['hsc'])
+        dsc = float(mydict['dsc'])
+        mba = float(mydict['mba'])
+        inputfeatures = [[gender, spec, tech, work, ssc, hsc, dsc, mba]]
+
+        # predicting the class either 0 or 1
+
+        predictedclass = rf.predict(inputfeatures)
+
+        # predicting the probability
+
+        predictedprob = rf.predict_proba(inputfeatures)
+
+        print(predictedclass, predictedprob[0][0])
+
+        if predictedclass[0] == 1:
+            proba = predictedprob[0][1]
+
+        else:
+            proba = predictedprob[0][0]
+
+        print(predictedclass, proba*100)
+
+        placemap = {1: 'Will be Placed', 0: 'Better Luck Next Time :('}
+        predictedclassres = placemap[predictedclass[0]]
+
+        if predictedclass[0] == 1:
+            return render_template('Result.html', predictedclasssend=predictedclassres, predictedprob=round(proba*100, 2), placed=True)
+
+        else:
+            return render_template('Result.html', predictedclasssend=predictedclassres)
+
+    return render_template('Home.html')
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
